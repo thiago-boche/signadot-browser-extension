b54e92516ec7095e3a98ed4ae871a6edb77d70d3
jmsktm <james.singh.np@gmail.com>
2023-11-14 10:09:36 -0800

    Use Chrome Extension Manifest V3 (MV3)

--- src/background.ts
+++ src/background.ts
@@ -1,51 +1,85 @@
 // background.ts
 
+type HeaderOperation = 'append' | 'set' | 'remove' | 'removeIfEmpty';
+
 const ROUTING_KEY = "routingKey";
-const ENABLED = "enabled";
+const ENABLED_KEY = "enabled";
 const ROUTING_KEY_HEADER_KEY = "uberctx-sd-routing-key";
 
 let inMemoryHeaderValue: string | undefined = undefined;
 let inMemoryFeatureEnabled: boolean = false;
 
+function updateDynamicRules() {
+  if (inMemoryFeatureEnabled) {
+    // Define the rule for modifying request headers.
+    const rule: chrome.declarativeNetRequest.Rule = {
+      "id": 1,
+      "priority": 1,
+      "action": {
+        "type": chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+        "requestHeaders": [
+          {
+            "header": ROUTING_KEY_HEADER_KEY,
+            "operation": chrome.declarativeNetRequest.HeaderOperation.SET,
+            "value": inMemoryHeaderValue || ''
+          },
+        ]
+      },
+      "condition": {
+        "urlFilter": "*",
+        "resourceTypes": [chrome.declarativeNetRequest.ResourceType.MAIN_FRAME]
+      }
+    };
+
+    // Update the dynamic rules
+    chrome.declarativeNetRequest.updateDynamicRules(
+      {
+        addRules: [rule], // Set the new rule
+        removeRuleIds: [1] // Remove the previous rule
+      }
+    ).then(() => {
+      // Adding console.log() to help with debugging (Should be fine to retain in published script)
+      chrome.declarativeNetRequest.getDynamicRules().then(rules => {
+        console.log(">> NEW DYNAMIC RULES: ", JSON.stringify(rules));
+      })
+    }).catch((error) => {
+      console.log('>> Error updating dynamic rule:', error);
+    });
+  } else {
+    // Remove the previously set rule
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [1]
+    });
+  }
+}
+
 // This function is used to update the in-memory values from local storage
-function updateInMemoryValues(caller: string): void {
-  chrome.storage.local.get([ROUTING_KEY, ENABLED], (result) => {
+function updateInMemoryValues(): void {
+  chrome.storage.local.get([ROUTING_KEY, ENABLED_KEY], (result) => {
     inMemoryHeaderValue = result[ROUTING_KEY];
-    inMemoryFeatureEnabled = !!result[ENABLED];
+    inMemoryFeatureEnabled = !!result[ENABLED_KEY];
+    updateDynamicRules();
   });
 }
 
 // Initialize the in-memory value when the extension is installed/updated or when Chrome is started
-chrome.runtime.onInstalled.addListener(() => updateInMemoryValues("onInstalled"));
-chrome.runtime.onStartup.addListener(() => updateInMemoryValues("onStartup"));
+chrome.runtime.onInstalled.addListener(() => updateInMemoryValues());
+chrome.runtime.onStartup.addListener(() => updateInMemoryValues());
 
 // Keep the in-memory value in sync with changes in storage
 chrome.storage.onChanged.addListener((changes, areaName) => {
   if (areaName === 'local') {
+    let updated = false;
     if (changes[ROUTING_KEY]) {
       inMemoryHeaderValue = changes[ROUTING_KEY]?.newValue;
+      updated = true;
     }
-    if (changes[ENABLED]) {
-      inMemoryFeatureEnabled = !!(changes[ENABLED]?.newValue);
+    if (changes[ENABLED_KEY]) {
+      inMemoryFeatureEnabled = !!(changes[ENABLED_KEY]?.newValue);
+      updated = true;
+    }
+    if (updated) {
+      updateDynamicRules();
     }
   }
 });
-
-// This listener modifies the request headers using the in-memory API key
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  (details) => {
-    const requestHeaders = details.requestHeaders || [];
-
-    if (inMemoryHeaderValue && inMemoryFeatureEnabled) {
-      const headerIndex = requestHeaders.findIndex(header => header.name.toLowerCase() === ROUTING_KEY_HEADER_KEY);
-      if (headerIndex !== -1) {
-        requestHeaders[headerIndex].value = inMemoryHeaderValue;
-      } else {
-        requestHeaders.push({ name: ROUTING_KEY_HEADER_KEY, value: inMemoryHeaderValue });
-      }
-    }
-    return { requestHeaders };
-  },
-  { urls: ['<all_urls>'] },
-  ['blocking', 'requestHeaders', 'extraHeaders']
-);
