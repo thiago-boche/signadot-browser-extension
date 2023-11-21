b0d0c844e976a2b8698317c4e65182ab9d05e6f3
jmsktm <james.singh.np@gmail.com>
2023-11-21 11:43:32 -0800

    Move out direct references to chrome storage to a custom hook

--- src/components/Frame/Frame.tsx
+++ src/components/Frame/Frame.tsx
@@ -1,106 +1,61 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./Frame.module.css";
-import { Switch } from "@blueprintjs/core";
-import { RoutingEntity } from "../ListRouteEntries/types";
+import {Switch} from "@blueprintjs/core";
+import {RoutingEntity} from "../ListRouteEntries/types";
 import ListRouteEntries from "../ListRouteEntries";
-import { useFetchRoutingEntries } from "../ListRouteEntries/hooks";
+import {useFetchRoutingEntries} from "../ListRouteEntries/hooks";
 import PinnedRouteGroup from "../PinnedRouteGroup";
-
-const ROUTING_KEY = "routingKey";
-const ENABLED = "enabled";
-
-interface StorageChange {
-  [key: string]: {
-    oldValue?: any;
-    newValue?: any;
-  };
-}
+import {useChromeStorage} from "../../hooks/storage";
 
 const Frame = () => {
-  const [enabled, setEnabled] = React.useState<boolean>(false);
-  const [debug, setDebug] = React.useState<boolean>(false);
+    const [debug, setDebug] = React.useState<boolean>(true);
+    const [routingKey, setRoutingKeyFn, enabled, setEnabledFn] = useChromeStorage();
+    const routingEntities: RoutingEntity[] = useFetchRoutingEntries();
+    const [userSelectedEntity, setUserSelectedEntity] = React.useState<RoutingEntity | undefined>(undefined);
 
-  const [storageValue, setStorageValue] = React.useState<string | undefined>(
-    undefined
-  );
+    React.useEffect(() => {
+        setRoutingKeyFn(userSelectedEntity?.routingKey)
+    }, [userSelectedEntity]);
+    const pinnedRoutingEntityData: RoutingEntity | undefined =
+        React.useMemo(() => {
+            const filteredList = routingEntities?.filter(
+                (entity) => entity.routingKey === routingKey
+            );
+            return filteredList?.[0];
+        }, [routingKey, routingEntities]);
 
-  const handleEnabledChange = (enable: boolean) => {
-    setEnabled(enable);
-    chrome.storage.local.set({
-      [ENABLED]: enable,
-    });
-  };
-
-  useEffect(() => {
-    chrome.storage.local.get([ROUTING_KEY, ENABLED], (result) => {
-      if (ROUTING_KEY in result) {
-        setStorageValue(result?.[ROUTING_KEY] || "");
-      }
-      setEnabled(!!result[ENABLED]);
-    });
-
-    const handleStorageChange = (changes: StorageChange, area: string) => {
-      if (area === "local" && ROUTING_KEY in changes) {
-        setStorageValue(changes[ROUTING_KEY].newValue || "");
-      }
-    };
-
-    // Add event listener for changes in storage
-    chrome.storage.onChanged.addListener(handleStorageChange);
-
-    // Cleanup function to remove event listener
-    return () => chrome.storage.onChanged.removeListener(handleStorageChange);
-  }, []);
-
-  const routingEntities: RoutingEntity[] = useFetchRoutingEntries();
-  const [userSelectedEntity, setUserSelectedEntity] = React.useState<
-    RoutingEntity | undefined
-  >(undefined);
-  React.useEffect(() => {
-    chrome.storage.local.set({
-      [ROUTING_KEY]: userSelectedEntity?.routingKey,
-    });
-  }, [userSelectedEntity]);
-  const pinnedRoutingEntityData: RoutingEntity | undefined =
-    React.useMemo(() => {
-      const filteredList = routingEntities?.filter(
-        (entity) => entity.routingKey === storageValue
-      );
-      return filteredList?.[0];
-    }, [storageValue, routingEntities]);
-
-  return (
-    <div>
-      <div className={styles.switchPanel}>
-        <Switch
-          alignIndicator={"right"}
-          label={`Setting Routing Key header is ${
-            enabled ? "ENABLED" : "DISABLED"
-          }`}
-          onChange={(e) => handleEnabledChange(e.target.checked)}
-          className={styles.switch}
-          checked={enabled}
-        />
-      </div>
-      {debug && (
+    return (
         <div>
-          <div>Selected entity: {JSON.stringify(userSelectedEntity)}</div>
-          <div>Stored Value: {storageValue}</div>
+            <div className={styles.switchPanel}>
+                <Switch
+                    alignIndicator={"right"}
+                    label={`Setting Routing Key header is ${
+                        enabled ? "ENABLED" : "DISABLED"
+                    }`}
+                    onChange={(e) => setEnabledFn(e.target.checked)}
+                    className={styles.switch}
+                    checked={enabled}
+                />
+            </div>
+            {debug && (
+                <div>
+                    <div>Selected entity: {JSON.stringify(userSelectedEntity)}</div>
+                    <div>Stored RoutingKey: {routingKey}</div>
+                </div>
+            )}
+            {enabled && (
+                <div className={styles.content}>
+                    <ListRouteEntries
+                        routingEntities={routingEntities}
+                        setUserSelectedRoutingEntity={setUserSelectedEntity}
+                    />
+                    {pinnedRoutingEntityData && (
+                        <PinnedRouteGroup routingEntity={pinnedRoutingEntityData}/>
+                    )}
+                </div>
+            )}
         </div>
-      )}
-      {enabled && (
-        <div className={styles.content}>
-          <ListRouteEntries
-            routingEntities={routingEntities}
-            setUserSelectedRoutingEntity={setUserSelectedEntity}
-          />
-          {pinnedRoutingEntityData && (
-            <PinnedRouteGroup routingEntity={pinnedRoutingEntityData} />
-          )}
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Frame;
---
+++ src/hooks/storage.ts
@@ -0,0 +1,53 @@
+import React from "react";
+import StorageChange = chrome.storage.StorageChange;
+
+enum StorageKey {
+    RoutingKey = "routingKey",
+    Enabled = "enabled"
+}
+
+type ChromeStorageHookOutput = [
+    routingKey: (string | undefined),
+    setRoutingKeyFn: ((value: (string | undefined)) => Promise<void>),
+    enabled: boolean,
+    setEnabled: ((value: boolean) => Promise<void>)
+]
+
+export const useChromeStorage = (): ChromeStorageHookOutput => {
+    const [routingKey, setRoutingKey] = React.useState<string | undefined>(undefined);
+    const [enabled, setEnabled] = React.useState<boolean>(false);
+
+    const setRoutingKeyFn = (value: string | undefined) => chrome.storage.local.set({[StorageKey.RoutingKey]: value})
+    const setEnabledFn = (value: boolean) => chrome.storage.local.set({[StorageKey.Enabled]: value})
+
+    React.useEffect(() => {
+            // Populate value for routingKey and enabled from Chrome Storage.
+            chrome.storage.local.get([StorageKey.RoutingKey, StorageKey.Enabled], (result) => {
+                if (StorageKey.RoutingKey in result) {
+                    setRoutingKey(result?.[StorageKey.RoutingKey]);
+                }
+                setEnabled(!!result[StorageKey.Enabled]);
+            });
+
+            // Update values for RoutingKey and enabled when the value in Google (Local) storage changes.
+            const handleStorageChange = (changes: { [p: string]: StorageChange }, area: string) => {
+                if (area === "local") {
+                    if (StorageKey.RoutingKey in changes) {
+                        setRoutingKey(changes[StorageKey.RoutingKey].newValue);
+                    }
+                    if (StorageKey.Enabled in changes) {
+                        setEnabled(!!changes[StorageKey.Enabled].newValue);
+                    }
+                }
+            }
+
+            // Add event listener for changes in storage
+            chrome.storage.onChanged.addListener(handleStorageChange);
+
+            // Cleanup function to remove event listener
+            return () => chrome.storage.onChanged.removeListener(handleStorageChange);
+        }, []
+    )
+
+    return [routingKey, setRoutingKeyFn, enabled, setEnabledFn];
+}
