3a5982b217b647cbe3f0d51797bbb58d7527690e
Anirudh Ramanathan <anirudh@signadot.com>
2024-01-05 08:16:18 -0800

    Update exclusions of header injection (#20)
    We don't want to inject headers when other components will be injecting
    headers already like the preview server.

--- src/background.ts
+++ src/background.ts
@@ -57,7 +57,7 @@ const getRules = (
               },
               condition: {
                 urlFilter: "*",
-                excludedRequestDomains: ["preview.signadot.com"],
+                excludedRequestDomains: ["preview.signadot.com", "preview.staging.signadot.com", "localhost.signadot.com"],
                 resourceTypes: MODIFY_HEADER_IN_RESOURCE_TYPES,
               },
             } as chrome.declarativeNetRequest.Rule)
